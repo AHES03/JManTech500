42270681e891c109dea95f2fc38dac271afc96e2
hadi <hadi2uk03@gmail.com>
2023-07-19 14:09:55 +0100

    With Part2

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import openpyxl
+import numpy as np
 import datetime
 from openpyxl import load_workbook
 
@@ -9,15 +10,56 @@ file_path = '/Users/hadi/PycharmProjects/JManTech500/slides_and_excel/230619 UpR
 workbook = openpyxl.load_workbook(file_path)
 
 # Select the first sheet (you can select other sheets by name if needed)
-sheet = workbook["Customers"]
+sheetCust = workbook["Customers"]
 CustomerDict={}
+counter=0
 # Iterate through the rows and columns and read the data
-for row in sheet.iter_rows(min_row=1, values_only=True):
+for row in sheetCust.iter_rows(min_row=1, values_only=True):
     if row[1] in CustomerDict:
-        CustomerDict[row[1]].append(row[0])
+        try:
+            CustomerDict[row[1]].append((round(row[0])))
+        except:
+            print(" ")
     else:
-        CustomerDict[row[1]]=[row[0]]
-print(CustomerDict)
+        if counter !=0:
+            CustomerDict[row[1]]=[round(row[0])]
+        else:
+            print(" ")
+        counter+=1
+SalesDict={}
+sheetSales = workbook["Sales"]
 
+for row in sheetSales.iter_rows(min_row=1, values_only=True):
+    try:
+        orderID=row[2]
+        year=str((row[4]).year)
+        SalesDict[orderID] = year
+    except:
+        print("error in type from row 1")
 # Close the workbook after reading
+
+bothTrue=0
+
+only2020=0
 workbook.close()
+TotalCustomers=(len(CustomerDict))
+for i in CustomerDict:
+    Bool2019 = False
+    Bool2020 = False
+    in2020 = False
+    OrdersFromCustomer=(len(CustomerDict[i]))
+
+    if OrdersFromCustomer>=1:
+        for x in (CustomerDict[i]):
+            if x in SalesDict:
+                year=(SalesDict[x])
+            if year=='2019':
+                Bool2019=True
+            elif year=='2020':
+                Bool2020=True
+    if Bool2019==True and Bool2020==True:
+        bothTrue+=1
+    elif Bool2019==False and Bool2020==True:
+        only2020+=1
+print((bothTrue/TotalCustomers)*100)
+print((only2020/TotalCustomers)*100)
