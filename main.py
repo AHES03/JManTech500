914c0a226bd663ecb0a2d620289b66857d010442
hadi <hadi2uk03@gmail.com>
2023-07-19 11:06:19 +0100

    Start

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-# This is a sample Python script.
+import openpyxl
+import datetime
+from openpyxl import load_workbook
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# Replace 'path/to/your/file.xlsx' with the actual path to your XLSX file
+file_path = '/Users/hadi/PycharmProjects/JManTech500/slides_and_excel/230619 UpReach Bootcamp PowerBI and Python Challenge.xlsx'
 
+# Load the Excel workbook
+workbook = openpyxl.load_workbook(file_path)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# Select the first sheet (you can select other sheets by name if needed)
+sheet = workbook["Customers"]
+CustomerDict={}
+# Iterate through the rows and columns and read the data
+for row in sheet.iter_rows(min_row=1, values_only=True):
+    if row[1] in CustomerDict:
+        CustomerDict[row[1]].append(row[0])
+    else:
+        CustomerDict[row[1]]=[row[0]]
+print(CustomerDict)
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# Close the workbook after reading
+workbook.close()
